5e64eb98b3e9a4738c79c27fddb99f5c7904cbc0
sirlancer <920365914@qq.com>
2018-06-19 10:25:21 +0800

    Algorithm

---
+++ List/合并链表.cpp
@@ -0,0 +1,30 @@
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};*/
+class Solution {
+public:
+    ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
+    {
+        if(pHead1 == nullptr){
+            return pHead2;
+        }
+        if(pHead2 == nullptr){
+            return pHead1;
+        }
+        ListNode* newHead = nullptr;
+        if(pHead1->val < pHead2->val){
+            newHead = pHead1;
+            newHead->next = Merge(pHead1->next, pHead2);
+        }else{
+            newHead = pHead2;
+            newHead->next = Merge(pHead1, pHead2->next);
+        }
+        return newHead;
+        
+    }
+};
